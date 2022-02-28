c35e8a98c696bb5ec377fbf29bfa591fc37d8965
lesrz <suarezarnaldi.leo@gmail.com>
2022-02-27 22:09:33 -0300

    Add data array for experiences section

---
+++ src/app/pages/home/hero/experiences.data.ts
@@ -0,0 +1,49 @@
+export const exps = [
+  {
+    img: 'assets/icons/experiences/integrity.svg',
+    title: 'Integridad',
+    description:
+      'Mostramos el más alto nivel de integridad en la forma en que llevamos a cabo nuestros servicios',
+  },
+  {
+    img: 'assets/icons/experiences/demonstrate.svg',
+    title: 'Demostración',
+    description: 'Demostramos una fuerte voluntad de triunfar en el mercado',
+  },
+  {
+    img: 'assets/icons/experiences/diversity.svg',
+    title: 'Diversidad',
+    description:
+      'Promovemos diversidad en el lugar de trabajo y en nuestra comunidad',
+  },
+  {
+    img: 'assets/icons/experiences/teamwork.svg',
+    title: 'Equipo',
+    description:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid praesentium explicabo',
+  },
+  {
+    img: 'assets/icons/experiences/collaboration.svg',
+    title: 'Colaboración',
+    description:
+      'Exhibiendo el más alto nivel de colaboración en la forma en que conducimos nuestros trabajos',
+  },
+  {
+    img: 'assets/icons/experiences/technology.svg',
+    title: 'Tecnología',
+    description:
+      'Utilizamos el poder de la tecnología para ofrecer una mejor experiencia al cliente',
+  },
+  {
+    img: 'assets/icons/experiences/corporate.svg',
+    title: 'Empresa',
+    description:
+      'Establecemos el estándar de la mejor Ciudadanía Corporativa en las comunidades en las que trabajamos',
+  },
+  {
+    img: 'assets/icons/experiences/digital.svg',
+    title: 'Digital',
+    description:
+      'Estableciendo el estándar del mejor ecosistema digital del mercado',
+  },
+];
